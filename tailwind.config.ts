fcdd4ca2f37df593ca19b33676c43793bc710493
josejmv <josejmvasquez@gmail.com>
2024-12-30 15:56:52 -0400

    chore: initialize repository

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        'primary-content': 'var(--primary-content)',
+        'secondary-content': 'var(--secondary-content)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
