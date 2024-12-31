4c3a3ae3694ec612d2b7a3d460c8cedc4ee47a93
josejmv <josejmvasquez@gmail.com>
2024-12-31 01:52:51 -0400

    feat: implement fetch all games and loader

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,22 +1,23 @@
+// components
+import { GamesView } from '@/app/_components/organisms/games-view/view'
+
 // types
+import type { SearchParamsDataType } from '@/app/_types'
 import type { NextPage } from 'next'
 
-const HomePage: NextPage = () => {
+type HomePageProps = {
+  searchParams: Promise<SearchParamsDataType>
+}
+
+const HomePage: NextPage<HomePageProps> = async (props) => {
+  const searchParams = await props.searchParams
+
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-        <ol className='list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
-          <li className='mb-2'>
-            Get started by editing{' '}
-            <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold'>
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
-    </div>
+    <article className='w-full max-w-screen-xl'>
+      <h1 className='font-bold text-4xl'>Top sellers</h1>
+
+      <GamesView params={searchParams} />
+    </article>
   )
 }
 
--- app/_services/get-games/index.ts
+++ app/_services/get-games/index.ts
@@ -1,3 +1,4 @@
+// types
 import type { SearchParamsDataType } from '@/app/_types'
 import type { GameDataType } from '@/app/_types/models/game'
 
@@ -5,15 +6,16 @@ import type { GameDataType } from '@/app/_types/models/game'
  * Get games from the API
  */
 export const getGames = async (params: SearchParamsDataType) => {
+  let url = '/api/games'
   const searchParams = new URLSearchParams()
   Object.entries(params).forEach(([key, value]) => {
     if (Array.isArray(value))
       value.forEach((val) => searchParams.append(key, val))
     else if (value !== undefined) searchParams.append(key, value)
   })
-  return fetch(`/api/games?${searchParams.toString()}`).then((res) =>
-    res.json()
-  ) as Promise<{
+  if (searchParams.toString().length) url = url + `?${searchParams.toString()}`
+
+  return fetch(url).then((res) => res.json()) as Promise<{
     totalPages: number
     currentPage: number
     games: GameDataType[]
